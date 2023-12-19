fd7f0cdc9c3ddd9452d17e22565e2a13ed9442b1
vendethiel <vendethiel@hotmail.fr>
2023-12-19 01:01:55 +0100

    begin PLoop version of Lua FauxCombinator

---
+++ src/Parser.lua
@@ -0,0 +1,66 @@
+local PLOOP_PATH = os.getenv("LUA_FAUX_COMBINATOR_PLOOP_PATH")
+if PLOOP_PATH then
+  package.path = package.path .. ";" .. PLOOP_PATH .. "?.lua;" .. PLOOP_PATH .. "?/init.lua"
+end
+
+-- TODO factor this out?
+PLOOP_PLATFORM_SETTINGS = {
+  --ENV_ALLOW_GLOBAL_VAR_BE_NIL = false,
+  OBJECT_NO_RAWSEST = true,
+  OBJECT_NO_NIL_ACCESS = true
+}
+
+require "PLoop"(function (_ENV)
+  namespace "FauxCombinator"
+
+  interface "IToken" (function (_ENV)
+    __Abstract__()
+    function GetType()
+    end
+  end)
+
+  -- TODO AllowObject?
+  -- https://github.com/kurapica/Scorpio/blob/c6e4fc39438bc7a8d429270a09a73e366c1de16e/Modules/Global.lua#L175-L183
+  class "BasicToken" (function (_ENV)
+    extend "IToken"
+    property "Type" { type = String }
+
+    function GetType(self)
+      return self.Type
+    end
+  end)
+
+  class "Parser" (function (_ENV)
+    property "Tokens" { type = Array[IToken] }
+    field { index = 1 }
+
+    function IsEOF(self)
+      return self.index > #self.Tokens
+    end
+
+    __Arguments__{}(IToken/nil)
+    function Peek(self)
+      if self:IsEOF() then
+        return
+      end
+      return self.Tokens[self.index]
+    end
+
+    __Arguments__{String}(IToken/nil)
+    function Maybe(self, type)
+      local token = self:Peek()
+      if token and token:GetType() == type then
+        self.index = self.index + 1
+        return token
+      end
+    end
+
+    __Arguments__{String}(IToken/nil)
+    function Expect(self, type)
+       if self:IsEOF() then
+         throw("FauxCombinator: Reading past EOF")
+       end
+       return self:Maybe(type) or throw("FauxCombinator: Wrong token type")
+    end
+  end)
+end)
---
+++ test/Parser.lua
@@ -0,0 +1,87 @@
+local PLOOP_PATH = os.getenv("LUA_FAUX_COMBINATOR_PLOOP_PATH")
+if PLOOP_PATH then
+  package.path = package.path .. ";" .. PLOOP_PATH .. "?.lua;" .. PLOOP_PATH .. "?/init.lua"
+end
+
+require "PLoop"
+require "PLoop.System.UnitTest"
+require "src/Parser"
+
+PLoop.System.Logger.Default:AddHandler(print)
+
+UnitTest "FauxCombinator.Parser"(function (_ENV)
+  local lparen = FauxCombinator.BasicToken{Type = "lparen"}
+  local rparen = FauxCombinator.BasicToken{Type = "rparen"}
+
+  class "IdToken"(function (_ENV)
+    extend "FauxCombinator.IToken"
+
+    property "Id" { type = String }
+
+    function GetType()
+      return "id"
+    end
+  end)
+
+  local id_a = IdToken{Id = "a"}
+  local id_b = IdToken{Id = "b"}
+  local id_c = IdToken{Id = "c"}
+  local id_d = IdToken{Id = "d"}
+
+  __Test__()
+  function TestPeekOK()
+    local tokens = Array[FauxCombinator.IToken]({lparen})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Equal(p:Peek().Type, 'lparen')
+  end
+
+  __Test__()
+  function TestPeekEOF()
+    local tokens = Array[FauxCombinator.IToken]({})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Nil(p:Peek())
+  end
+
+  __Test__()
+  function TestMaybeOK()
+    local tokens = Array[FauxCombinator.IToken]({lparen, rparen})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Equal(p:Maybe('lparen').Type, 'lparen')
+    Assert.Equal(p:Maybe('rparen').Type, 'rparen')
+  end
+
+  __Test__()
+  function TestMaybeKO()
+    local tokens = Array[FauxCombinator.IToken]({lparen})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Nil(p:Maybe('rparen'))
+  end
+
+  __Test__()
+  function TestMaybeEOF()
+    local tokens = Array[FauxCombinator.IToken]({})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Nil(p:Maybe('rparen'))
+  end
+  
+  __Test__()
+  function TestExpectOK()
+    local tokens = Array[FauxCombinator.IToken]({lparen, rparen})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Equal(p:Expect('lparen').Type, 'lparen')
+    Assert.Equal(p:Expect('rparen').Type, 'rparen')
+  end
+
+  __Test__()
+  function TestExpectKOType()
+    local tokens = Array[FauxCombinator.IToken]({lparen})
+    local p = FauxCombinator.Parser{Tokens = tokens}
+    Assert.Error(function ()
+      Assert.Equal(p:Expect('rparen').Type, 'rpare123n')
+    end)
+  end
+  -- TODO ExceptKOType
+  -- TODO ExceptKOEOF
+end)
+
+UnitTest("FauxCombinator.Parser"):Run()
